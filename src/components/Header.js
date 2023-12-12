051856d1a449ca4f8de388ecc07ab9643c1655aa
Anitta15 <152918187+Anitta15@users.noreply.github.com>
2023-12-12 16:21:58 +0530

    Update Header.js, header form updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,61 @@ function Header() {
       dispatch(logout())
   }
   return (
+      <Navbar expand="lg" bg="lightpink" variant="dark" collapseOnSelect>
+    <Container fluid>
+      <LinkContainer to='/'>
+      <Navbar.Brand >Cosmic Grab</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+     
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+<LinkContainer to='/cart'>
+  <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+</LinkContainer>
+{userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                                </NavDropdown>
+                            ) : (
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                                    </LinkContainer>
+                                )}
+                              
+
+              {userInfo && userInfo.isAdmin && (
+                                <NavDropdown title='Admin' id='adminmenue'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                </NavDropdown>
+                            )}
+   
+   <SearchBox/>
+
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 
 
   )
