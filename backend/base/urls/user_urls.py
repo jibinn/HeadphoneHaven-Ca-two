d492cba86c4ff4f0f4b9e57e7426dd7ee466dd78
gauthambinoy <153606915+gauthambinoy@users.noreply.github.com>
2023-12-12 12:23:12 +0000

    Update user_urls.py

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -1,1 +1,15 @@
+from django.urls import path
+from base.views import user_views as views
 
+
+urlpatterns=[
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/',views.getUserProfile,name="users-profile"),
+     path('profile/update/',views.updateUserProfile,name="users-profile-update"),
+    path('',views.getUsers,name="users"),
+    path('register/',views.registerUser,name="register"),
+    path('delete/<str:pk>/',views.deleteUser,name='user-delete'),
+    
+     path('<str:pk>/', views.getUserById, name='user'),
+    path('update/<str:pk>/',views.updateUser,name='user-update'),
+]
