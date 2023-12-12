5d266bf57b722e9073b32b5d3e047414b9e296c9
KrishnaAmal <83944816+KrishnaAmal@users.noreply.github.com>
2023-12-12 10:47:27 +0000

    Update user_views.py , user profile updated, check

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -56,7 +56,22 @@ def registerUser(request):
 def getRoutes(request):
     return Response('hello')
 
-
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer =UserSerializerWithToken(user,many=False)
+    data= request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    
+    if data['password'] != '':
+        user.password =make_password(data['password'])
+        
+    user.save()   
+    
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
