1bd69db3b7c8a5ca9752c78fca9b973132826201
KrishnaAmal <83944816+KrishnaAmal@users.noreply.github.com>
2023-12-12 10:31:39 +0000

    Update product_views   , image upload code

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -98,7 +98,14 @@ def deleteProduct(request,pk):
     product.delete()
     return Response('product deleted')
 
-
+@api_view(['POST'])
+def uploadimage(request):
+    data =request.data
+    product_id=data['product_id']
+    product= Product.objects.get(_id=product_id)
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image was Uploaded')
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
