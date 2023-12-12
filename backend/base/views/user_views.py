129a229cbb1f2588c11f35ea457c94299972d325
KrishnaAmal <83944816+KrishnaAmal@users.noreply.github.com>
2023-12-12 10:44:24 +0000

    Update user_views.py, register user code - please check team

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -32,7 +32,22 @@ class MyTokenObtainPairView(TokenObtainPairView):
         serializer_class =MyTokenObtainPairSerializer
         
 
-    
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except IntegrityError:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)   
 
 
         
