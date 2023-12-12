30c73f9934cc692740e9c928777339614420f43b
Anitta15 <152918187+Anitta15@users.noreply.github.com>
2023-12-12 16:16:03 +0530

    Update CosmicLoginScreen.js

--- src/Screens/CosmicLoginScreen.js
+++ src/Screens/CosmicLoginScreen.js
@@ -35,45 +35,7 @@ function CosmicLoginScreen() {
     };
   
     return (
-        <FormContainer className="login-container">
-            <h1 className="text-center mb-4">Sign In</h1>
-            {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
-            <Form onSubmit={submitHandler}>
-                <Form.Group controlId='email'>
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type='email'
-                        placeholder='Enter Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="form-control" 
-                    />
-                </Form.Group>
-                <Form.Group controlId='password'>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Enter Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="form-control" 
-                    />
-                </Form.Group>
-                <Button id="yourButtonId" type='submit' variant='primary' className="submit-button w-100">
-    Sign In
-</Button>
-
-            </Form>
-            <Row className='py-3'>
-                <Col>
-                    New Customer?{' '}
-                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-                        Register
-                    </Link>
-                </Col>
-            </Row>
-        </FormContainer>
+       
     );
 }
 export default CosmicLoginScreen;
