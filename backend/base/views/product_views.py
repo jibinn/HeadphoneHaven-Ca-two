8588c009c442b4e7c4f77f225daff9c8cbf6a2c8
KrishnaAmal <83944816+KrishnaAmal@users.noreply.github.com>
2023-12-12 10:19:31 +0000

    Update product_views.py

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -34,10 +34,7 @@ def getProducts(request):
 
 
 @api_view(['GET'])
-def getTopProducts(request):
-    products = Product.objects.filter(rating__gte=4).order_by('-rating')[:5]
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+
 
 
 # Retrieve a single product by its primary key
